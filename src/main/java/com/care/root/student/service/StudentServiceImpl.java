c710ea787686bcc58cbe60485d1f90985e441201
Hyunwoo-Cho89 <paul_jo@naver.com>
2021-06-17 10:19:47 +0900

    코드 정리

--- src/main/java/com/care/root/student/service/StudentServiceImpl.java
+++ src/main/java/com/care/root/student/service/StudentServiceImpl.java
@@ -22,8 +22,10 @@ import com.care.root.student.dto.TuitionInfoDTO;
 import com.care.root.student.dto.TuitionPayDTO;
 
 @Service
-public class StudentServiceImpl implements StudentService{
-	@Autowired StudentDAO mapper;
+public class StudentServiceImpl implements StudentService {
+	@Autowired
+	StudentDAO mapper;
+
 	@Override
 	public void studentInfo(Model model, String id) {
 
@@ -32,11 +34,12 @@ public class StudentServiceImpl implements StudentService{
 			String birthDate = dto.getResidentNum().substring(0, 6);
 			dto.setBirthDate(birthDate);
 			model.addAttribute("info", dto);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
+
 	@Override
 	public void subjectInfo(Model model, String id) {
 
@@ -45,26 +48,26 @@ public class StudentServiceImpl implements StudentService{
 			String birthDate = dto.getResidentNum().substring(0, 6);
 			dto.setBirthDate(birthDate);
 			ArrayList<SubjectDTO> list = null;
-			if(dto.getGradeSemester() == dto.getClassReq()) {
+			if (dto.getGradeSemester() == dto.getClassReq()) {
 				list = mapper.subjectInfoA(dto.getGradeSemester(), dto.getMajor());
-			}else {
+			} else {
 				list = mapper.subjectInfoB(dto.getGradeSemester(), dto.getMajor());
 			}
-			
+
 			model.addAttribute("info", dto);
 			model.addAttribute("subject", list);
-			if(list.size() == 0) {
+			if (list.size() == 0) {
 				model.addAttribute("repeat", 1);
-			}else {
+			} else {
 				model.addAttribute("repeat", list.size());
 			}
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	@Override
 	public void tuitionInfo(Model model, String id) {
 		try {
@@ -72,13 +75,12 @@ public class StudentServiceImpl implements StudentService{
 			TuitionInfoDTO tuitionDTO = mapper.tuitionInfo(id);
 			model.addAttribute("studentInfo", infoDTO);
 			model.addAttribute("tuitionInfo", tuitionDTO);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 
 	}
-	
+
 	@Override
 	public String tuitionPayment(String id, HttpServletRequest request) {
 		String message = null;
@@ -90,75 +92,76 @@ public class StudentServiceImpl implements StudentService{
 		payDTO.setBank(request.getParameter("bankSelect"));
 		payDTO.setAccount(request.getParameter("account"));
 		result = mapper.tutionPayMent(payDTO);
-		
-		if(result == 1) {
-			if(infoDTO.getGradeSemester()==101) {
+
+		if (result == 1) {
+			if (infoDTO.getGradeSemester() == 101) {
 				mapper.tuitionSaveA(id);
-			}else if(infoDTO.getGradeSemester()==102){
+			} else if (infoDTO.getGradeSemester() == 102) {
 				mapper.tuitionSaveB(id);
-			}
-			else if(infoDTO.getGradeSemester()==201) {
+			} else if (infoDTO.getGradeSemester() == 201) {
 				mapper.tuitionSaveC(id);
-			}
-			else if(infoDTO.getGradeSemester()==202) {
+			} else if (infoDTO.getGradeSemester() == 202) {
 				mapper.tuitionSaveD(id);
 			}
-			
+
 		}
 		System.out.println(id);
 		System.out.println(request.getParameter("bankSelect"));
 		System.out.println(request.getParameter("account"));
 		System.out.println(result);
-		
-		
-		
-			/*
-			 * if(result == 1) { message = "<script>alert('계좌번호가 정상적으로 등록되었습니다.');"; message
-			 * = "location.href=('tuituinChk');</script>"; }else { message =
-			 * "<script>alert('계좌번호가 등록에 실패하였습니다.');</script>"; }
-			 */
+
+		/*
+		 * if(result == 1) { message = "<script>alert('계좌번호가 정상적으로 등록되었습니다.');"; message
+		 * = "location.href=('tuituinChk');</script>"; }else { message =
+		 * "<script>alert('계좌번호가 등록에 실패하였습니다.');</script>"; }
+		 */
 		return message;
 	}
+
 	@Override
 	public void classRequest(Model model, String id) {
 		StudentInfoDTO infoDTO = mapper.studentInfo(id);
-		ArrayList<SubjectDTO> list =  mapper.subjectInfoC(infoDTO.getGradeSemester(), infoDTO.getMajor());
+		ArrayList<SubjectDTO> list = mapper.subjectInfoC(infoDTO.getGradeSemester(), infoDTO.getMajor());
 		model.addAttribute("info", infoDTO);
 		model.addAttribute("list", list);
-		
+
 	}
+
 	@Override
 	public void classReqChk(String id) {
 		StudentInfoDTO infoDTO = mapper.studentInfo(id);
 		mapper.classReq(id, infoDTO.getGradeSemester());
 	}
+
 	@Override
 	public void gradeInquiry(Model model, String id) {
 		StudentInfoDTO infoDTO = mapper.studentInfo(id);
-		List<GradeDTO>  list = mapper.gradeInfo(id, infoDTO.getGradeSemester());
-		if(list.size() == 0) {
+		List<GradeDTO> list = mapper.gradeInfo(id, infoDTO.getGradeSemester());
+		if (list.size() == 0) {
 			model.addAttribute("repeat", 1);
-		}else {
+		} else {
 			model.addAttribute("repeat", list.size());
 		}
 		model.addAttribute("info", infoDTO);
 		model.addAttribute("grade", list);
-		
+
 	}
+
 	@Override
 	public List<GradeDTO> detailGrade(int semester, String id) {
 
-		
 		return mapper.gradeInfo(id, semester);
 	}
+
 	@Override
 	public void totalInquiry_G(Model model, String id) {
 		int applicationCred = 0;
-		int receivedCred = 0;;
+		int receivedCred = 0;
+		;
 		double avgGrade = 0;
 		String avgGrade2 = null;
 		int rank = 1;
-		
+
 		int tApplicationCred = 0;
 		int tReceivedCred = 0;
 		double tAvgGrade = 0;
@@ -167,16 +170,15 @@ public class StudentServiceImpl implements StudentService{
 		String tScore2 = null;
 		RegisterInfoDTO registerDTO = null;
 		DecimalFormat frmt = new DecimalFormat();
-		
+		List<GradeDTO> list = null;
+
 		try {
 			StudentInfoDTO infoDTO = mapper.studentInfo(id);
 			String birthDate = infoDTO.getResidentNum().substring(0, 6);
 			infoDTO.setBirthDate(birthDate);
 			model.addAttribute("info", infoDTO);
-		
-			List<GradeDTO> list = null;
-			
-			for(int i=1;i<=4; i++) {
+
+			for (int i = 1; i <= 4; i++) {
 				applicationCred = 0;
 				receivedCred = 0;
 				avgGrade = 0;
@@ -184,83 +186,79 @@ public class StudentServiceImpl implements StudentService{
 
 				registerDTO = null;
 				list = null;
-				if(i==1) {
+				if (i == 1) {
 					semester = 101;
-				}else if(i==2) {
+				} else if (i == 2) {
 					semester = 102;
-				}else if (i==3) {
+				} else if (i == 3) {
 					semester = 201;
-				}else {
+				} else {
 					semester = 202;
 				}
 				registerDTO = mapper.registerInfo(id, semester);
 				list = mapper.gradeInfo(id, semester);
-				
-				if(list.size() != 0) {
 
-				for (GradeDTO gradeDTO : list) {
-					applicationCred += gradeDTO.getApplicationCred();
-					receivedCred += gradeDTO.getReceivedCred();
-					if(gradeDTO.getScore() >= 95){
-						avgGrade += 4.5;
-				}else if(gradeDTO.getScore() < 95 && gradeDTO.getScore() >= 90) {
-						avgGrade += 4.0;
-				}else if(gradeDTO.getScore() < 90 && gradeDTO.getScore() >= 85) {
-						avgGrade += 3.5;
-				}else if(gradeDTO.getScore() < 85 && gradeDTO.getScore() >= 80) {
-						avgGrade += 3.0;
-				}else if(gradeDTO.getScore() < 80 && gradeDTO.getScore() >= 75) {
-					avgGrade += 2.5;
-				}else if(gradeDTO.getScore() < 75 && gradeDTO.getScore() >= 70) {
-					avgGrade += 2.0;
-				}else if(gradeDTO.getScore() < 70 && gradeDTO.getScore() >= 65) {
-					avgGrade += 1.0;
-				}else {
-					avgGrade += 0.0;
+				if (list.size() != 0) {
+
+					for (GradeDTO gradeDTO : list) {
+						applicationCred += gradeDTO.getApplicationCred();
+						receivedCred += gradeDTO.getReceivedCred();
+						if (gradeDTO.getScore() >= 95) {
+							avgGrade += 4.5;
+						} else if (gradeDTO.getScore() < 95 && gradeDTO.getScore() >= 90) {
+							avgGrade += 4.0;
+						} else if (gradeDTO.getScore() < 90 && gradeDTO.getScore() >= 85) {
+							avgGrade += 3.5;
+						} else if (gradeDTO.getScore() < 85 && gradeDTO.getScore() >= 80) {
+							avgGrade += 3.0;
+						} else if (gradeDTO.getScore() < 80 && gradeDTO.getScore() >= 75) {
+							avgGrade += 2.5;
+						} else if (gradeDTO.getScore() < 75 && gradeDTO.getScore() >= 70) {
+							avgGrade += 2.0;
+						} else if (gradeDTO.getScore() < 70 && gradeDTO.getScore() >= 65) {
+							avgGrade += 1.0;
+						} else {
+							avgGrade += 0.0;
+						}
+
+					}
+
+					tApplicationCred += applicationCred;
+					tReceivedCred += receivedCred;
+					tAvgGrade += avgGrade;
+
+					SemesterGradeDTO SgradeDTO = new SemesterGradeDTO();
+					avgGrade = avgGrade / (applicationCred / 3);
+					avgGrade2 = frmt.format(avgGrade);
+
+					SgradeDTO.setApplicationCred(applicationCred);
+					SgradeDTO.setReceivedCred(receivedCred);
+					SgradeDTO.setAvgGrade(avgGrade2);
+					SgradeDTO.setRank(registerDTO.getRank());
+					model.addAttribute("grade"+semester, SgradeDTO);
 				}
-				
 			}
-				
-				tApplicationCred += applicationCred;
-				tReceivedCred += receivedCred;
-				tAvgGrade += avgGrade;
-				
-				SemesterGradeDTO SgradeDTO = new SemesterGradeDTO();
-				avgGrade = avgGrade/(applicationCred/3);
-				avgGrade2 = frmt.format(avgGrade);
-				
-				SgradeDTO.setApplicationCred(applicationCred);
-				SgradeDTO.setReceivedCred(receivedCred);
-				SgradeDTO.setAvgGrade(avgGrade2);
-				SgradeDTO.setRank(registerDTO.getRank());
-				model.addAttribute("grade"+semester, SgradeDTO);
-			}
-			}
-			
-			
-			
-			
-			//total
+
+			// total
 			TotalGradeDTO tGradeDTO = new TotalGradeDTO();
-			tAvgGrade = tAvgGrade/(tApplicationCred/3);
+			tAvgGrade = tAvgGrade / (tApplicationCred / 3);
 			tAvgGrade2 = frmt.format(tAvgGrade);
-			
-			tScore = (tScore-tAvgGrade)*10;
-			tScore = 99-tScore;
+
+			tScore = (tScore - tAvgGrade) * 10;
+			tScore = 99 - tScore;
 			tScore2 = frmt.format(tScore);
-			
+
 			tGradeDTO.settApplicationCred(tApplicationCred);
 			tGradeDTO.settReceivedCred(tReceivedCred);
 			tGradeDTO.settAvgGrade(tAvgGrade2);
 			tGradeDTO.settScore(tScore2);
-			
+
 			model.addAttribute("total", tGradeDTO);
-				
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-	}
 
+	}
 
 }
